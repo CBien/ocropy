4676b38f1216a611f069207f2ee6788fd49a5a7a
Emile <emile@cbien.com>
2016-05-25 16:43:38 +0200

    rm 'en-default.pyrnn.gz' in setup.py (breaks pip-install)

--- setup.py
+++ setup.py
@@ -5,18 +5,17 @@ import sys,time,urllib,traceback,glob,os,os.path
 assert sys.version_info[0]==2 and sys.version_info[1]>=7,\
     "you must install and use OCRopus with Python version 2.7 or later, but not Python 3.x"
 
-from distutils.core import setup #, Extension, Command
-#from distutils.command.install_data import install_data
+from distutils.core import setup
+
 
 if not os.path.exists("models/en-default.pyrnn.gz"):
     print
-    print "You must download the default model 'en-default.pyrnn.gz'"
-    print "and put it into ./models."
+    print "Warning : you will need to download the model 'en-default.pyrnn.gz'"
+    print "and put it into ./models if you want to use it as default model."
     print
     print "Check https://github.com/tmbdev/ocropy for the location"
     print "of model files."
     print
-    sys.exit(1)
 
 scripts = [c for c in glob.glob("ocropus-*") if "." not in c and "~" not in c]
 
@@ -26,6 +25,5 @@ setup(
     author = "Thomas Breuel",
     description = "The OCRopy RNN-based Text Line Recognizer",
     packages = ["ocrolib"],
-    data_files= [('share/ocropus', ["models/en-default.pyrnn.gz"])],
     scripts = scripts,
     )
